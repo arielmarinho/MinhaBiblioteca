dc7deab86c247d9ae45d15a8be927cbf65008eec
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-03 21:03:03 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrmlocal/minhabiblioteca/Calculadora.kt
@@ -0,0 +1,10 @@
+package com.example.logonrmlocal.minhabiblioteca
+
+class Calculadora{
+    fun somar(v1: Int, v2: Int): Int{
+        return v1 + v2
+    }
+    fun subitrair(v1: Int, v2: Int): Int{
+        return v1 - v2
+    }
+}
